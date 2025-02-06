ac751980bf225be001c68a5a28821fae880c96df
Hamzq ait Mezouar <hamza@areyouquick.com>
2025-02-06 18:37:49 +0100

    work on errors handling

---
+++ lib/data/repositories/token/token_repository.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:diary/core/constants/urls.dart';
+import 'package:diary/core/errors/errors.dart';
+import 'package:diary/core/helpers/either_error_handler.dart';
+import 'package:diary/data/datasource/token/token_datasource.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import '../../../core/helpers/secure_storage_helper.dart';
+
+abstract class TokenRepository {
+  Future<Either<Failure, TokenResponse>> refreshToken();
+}
+
+class TokenRepositoryImpl extends TokenRepository {
+  final TokenRemoteDataSource _dataSource;
+  final ExceptionsHandler _exceptionsHandler;
+  final SecureStorageHelper _storageHelper;
+  TokenRepositoryImpl(this._dataSource, this._exceptionsHandler, this._storageHelper);
+  @override
+  Future<Either<Failure, TokenResponse>> refreshToken() async {
+    try {
+      String? refreshToken = await _storageHelper.getRefreshToken();
+      if (refreshToken == null) return Left(CacheFailure(errorMessage: "Something went wrong"));
+      TokenResponse response =
+          await _exceptionsHandler.dioExceptionsHandler(() => _dataSource.refreshToken(refreshToken));
+      return Right(response);
+    } catch (e) {
+      return Left(Failure(errorMessage: e.toString()));
+    }
+  }
+}
