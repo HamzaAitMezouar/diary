cdf91172ba310af0ca5e6fab057333c3b4990384
Hamzq ait Mezouar <hamza@areyouquick.com>
2025-02-10 21:56:20 +0100

    add articles card

--- lib/data/datasource/aricles/articel.dart
+++ lib/data/datasource/aricles/articel.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:diary/data/datasource/aricles/article_model.dart';
 import 'package:dio/dio.dart';
@@ -9,11 +10,12 @@ class ArticlesData {
     try {
       var dio = Dio();
       var response = await dio.request(
-        'https://newsapi.org/v2/everything?q=tesla&from=2025-01-09&sortBy=publishedAt&apiKey=2d1c1b32f5784a2d93e50f8e7b769706',
+        'https://newsapi.org/v2/everything?q=tesla&from=2025-02-09&sortBy=publishedAt&apiKey=2d1c1b32f5784a2d93e50f8e7b769706',
         options: Options(
           method: 'GET',
         ),
       );
+      log(response.data.toString());
 
       if (response.statusCode == 200) {
         for (var article in response.data["articles"]) {
@@ -22,6 +24,7 @@ class ArticlesData {
       }
       return articles;
     } catch (e) {
+      log(e.toString());
       return articles;
     }
   }
